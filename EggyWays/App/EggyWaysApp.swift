03b04d1f65ec91d615249ea8261a2314964ca2fb
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-29 14:22:02 +0200

    init

---
+++ EggyWays/App/EggyWaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggyWaysApp.swift
+//  EggyWays
+//
+//  Created by Serhii Babchuk on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct EggyWaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EWLaunchView()
+        }
+    }
+}
